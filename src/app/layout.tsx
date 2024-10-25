daeee38ed6ea82bc0cf6842a6f7aac264ea81791
D_Zucho <danny.nico.dev@outlook.com>
2024-10-24 23:53:07 -0700

    initial + setup for tailwind and prismic connection

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+import "./globals.css";
+import { DM_Sans } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${dmSans.variable}`}>
+      <body>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
