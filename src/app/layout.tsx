ccd50ce275d78d2fba299a3fc8619375954c879e
D_Zucho <danny.nico.dev@outlook.com>
2024-10-26 03:36:29 -0700

    add footer and initial header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 import "./globals.css";
 import { DM_Sans } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 // If loading a variable font, you don't need to specify the font weight
 const dmSans = DM_Sans({
@@ -17,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${dmSans.variable}`}>
-      <body>{children}</body>
+      <body className="bg-[#070815] text-white">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
